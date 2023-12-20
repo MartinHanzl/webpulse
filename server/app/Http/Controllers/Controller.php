c2a1f6360d29615f795b245d2c9eeb01fddc20b9
martashanzl2000 <martin.hanzl@monstermedia.cz>
2023-12-20 15:17:58 +0100

    migrations update

--- server/app/Http/Controllers/Controller.php
+++ server/app/Http/Controllers/Controller.php
@@ -5,8 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\App;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function handleLang(string $lang = null)
+    {
+        if ($lang == null) {
+            $lang = App::getLocale();
+        }
+
+        App::setLocale($lang);
+
+        return $lang;
+    }
 }
