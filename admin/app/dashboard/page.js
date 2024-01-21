87de29f3511b203b9027576c94e18297c27e301e
martashanzl2000 <martin.hanzl@monstermedia.cz>
2024-01-21 11:59:36 +0100

    layout update

--- admin/app/dashboard/page.js
+++ admin/app/dashboard/page.js
@@ -1,20 +1,4 @@
 'use client'
-// In pages/admin.js or similar
-export const getServerSideProps = async (context) => {
-    const { req } = context;
-    const token = req.cookies.token; // Assuming the token is stored in a cookie
-
-    if (!token) {
-        return {
-            redirect: {
-                destination: '/login',
-                permanent: false,
-            },
-        };
-    }
-
-    return { props: {} };
-};
 
 export default function Page() {
     return (
