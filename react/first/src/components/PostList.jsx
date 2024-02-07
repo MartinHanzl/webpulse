bd9da20e4ebeb4977f7d0795a74d7a7d31b640f6
martashanzl2000 <martin.hanzl@monstermedia.cz>
2024-02-07 19:02:25 +0100

    27. the special children prop

--- react/first/src/components/PostList.jsx
+++ react/first/src/components/PostList.jsx
@@ -3,6 +3,7 @@ import {useState} from "react";
 import Post from "./Post.jsx";
 import NewPost from "./NewPost.jsx";
 import styles from './PostList.module.css';
+import Modal from "./Modal.jsx";
 
 function PostList() {
     const [body, setBody] = useState('');
@@ -11,13 +12,19 @@ function PostList() {
     function bodyChangeHandler(event) {
         setBody(event.target.value);
     }
+
     function authorChangeHandler(event) {
         setAuthor(event.target.value);
     }
 
     return (
         <>
-            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
+            <Modal>
+                <NewPost
+                    onBodyChange={bodyChangeHandler}
+                    onAuthorChange={authorChangeHandler}
+                />
+            </Modal>
             <ul className={styles.ul}>
                 <Post author={author} body={body}/>
                 <Post author="Petr" body="Nevím"/>
