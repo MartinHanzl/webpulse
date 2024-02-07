e4a1232ad6a62411bbb433401cc6cadcfdfa8430
martashanzl2000 <martin.hanzl@monstermedia.cz>
2024-02-07 18:08:28 +0100

    react state hook

--- README.md
+++ README.md
@@ -3,13 +3,14 @@
 ## Instalace
 
 ## React refresher
-- npm create vite (react, javascript
+- npm create vite (react, javascript)
 - extension .jsx = having HTML code in javascript files
 - where we use components, it is required to start with uppercase character
 - if you have multiple elements side by side, it is necessary to wrap them by another element (for example: `<></>`)
 - in `return()` we can return only one element
 - we can pass function to props same as we can pass numbers and strings
-- 
+- function starting with *use* are considered as hooks
+- `useState()` always returns 2 elements as array (`useState()[0]` = current value, `useState()[1]` = updating value)
 
 ## Nápady
 - [ ] v administraci při vytváření nového záznamu nejdříve vytvořit skrze modal a potom přesměrovat na celkovou úpravu
--- react/first/src/components/NewPost.jsx
+++ react/first/src/components/NewPost.jsx
@@ -1,19 +1,23 @@
 import classes from './NewPost.module.css';
+import {useState} from "react";
 
 function NewPost() {
+    const [body, setBody] = useState('');
+
     function changeBodyHandler() {
-        console.log(event.target.value);
+        setBody(event.target.value);
     }
 
     return (
         <form className={classes.form}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+                <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
             </p>
+            <p>{body}</p>
             <p>
                 <label htmlFor="name">Your name</label>
-                <input type="text" id="name" required />
+                <input type="text" id="name" required/>
             </p>
         </form>
     );
