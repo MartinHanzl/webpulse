d0e21cea4cbd7e75b66522fff7a1da2fafc261d2
martashanzl2000 <martin.hanzl@monstermedia.cz>
2024-02-21 19:06:31 +0100

    124. Adding Server-Side Input Validation

--- foodies/lib/actions.js
+++ foodies/lib/actions.js
@@ -3,6 +3,10 @@
 import {saveMeal} from "@/lib/meals";
 import {redirect} from "next/navigation";
 
+function isInvalidText(text) {
+    return !text || text.trim() === '';
+}
+
 export async function shareMeal(FormData) {
     const meal = {
         title: FormData.get('title'),
@@ -11,6 +15,18 @@ export async function shareMeal(FormData) {
         creator: FormData.get('name'),
         instructions: FormData.get('instructions'),
         image: FormData.get('image')
+    }
+
+    if (
+        isInvalidText(meal.title) ||
+        isInvalidText(meal.summary) ||
+        isInvalidText(meal.instruction) ||
+        isInvalidText(meal.creator) ||
+        isInvalidText(meal.creator_email) ||
+        !meal.creator_email.includes('@') ||
+        !meal.image || meal.image.size === 0
+    ) {
+        throw new Error('Invalid input');
     }
 
     await saveMeal(meal);
