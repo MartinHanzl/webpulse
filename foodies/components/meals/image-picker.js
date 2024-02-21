c89440bbbe4b318bf05f577eef661874117dfccd
martashanzl2000 <martin.hanzl@monstermedia.cz>
2024-02-21 18:08:45 +0100

    117. Adding an Image Preview to the Picker

--- foodies/components/meals/image-picker.js
+++ foodies/components/meals/image-picker.js
@@ -1,17 +1,39 @@
 'use client';
 import styles from './image-picker.module.css';
-import {useRef} from "react";
+import {useRef, useState} from "react";
+import Image from "next/image";
 
 export default function ImagePicker(props) {
-    const imageInput = useRef('');
+    const [pickedImage, setPickedImage] = useState();
+    const imageInput = useRef();
+
     function handlePickClick() {
         imageInput.current.click();
+    }
+
+    function handleImageChange(event) {
+        const file = event.target.files[0];
+
+        if (!file) {
+            return;
+        }
+
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result);
+        };
+        fileReader.readAsDataURL(file);
     }
 
     return <div className={styles.picker}>
         <label htmlFor={props.name}>{props.label}</label>
         <div className={styles.controls}>
-            <input className={styles.input} type="file" id="image" accept="image/png, image/jpeg" name={props.name} ref={imageInput}></input>
+            <div className={styles.preview}>
+                {!pickedImage && <p>No image!</p>}
+                {pickedImage && <Image src={pickedImage} alt="Image selected by the user" fill/>}
+            </div>
+            <input className={styles.input} type="file" id="image" accept="image/png, image/jpeg" name={props.name}
+                   ref={imageInput} onChange={handleImageChange}></input>
             <button className={styles.button} type="button" onClick={handlePickClick}>Upload image</button>
         </div>
     </div>
