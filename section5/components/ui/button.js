9d500514c27ca9045a6a0d818a6137e34ba21c24
martashanzl2000 <martin.hanzl@monstermedia.cz>
2024-02-22 20:07:00 +0100

    164. Adding a Filter Form for Filtering Events

--- section5/components/ui/button.js
+++ section5/components/ui/button.js
@@ -2,9 +2,17 @@ import Link from "next/link";
 import styles from './button.module.css';
 
 export default function Button(props) {
-    return (
-        <Link href={props.link} className={styles.btn}>
-            {props.children}
-        </Link>
-    )
+    if (props.link) {
+        return (
+            <Link href={props.link} className={styles.btn}>
+                {props.children}
+            </Link>
+        )
+    } else {
+        return (
+            <button onClick={props.onClick} className={styles.btn}>
+                {props.children}
+            </button>
+        )
+    }
 }
--- section5/pages/events/index.js
+++ section5/pages/events/index.js
@@ -1,11 +1,13 @@
 import {getAllEvents} from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
 export default function EventsPage() {
     const events = getAllEvents();
     return (
         <main>
             <h1>Events page</h1>
+            <EventsSearch />
             <EventList items={events} />
         </main>
     )
