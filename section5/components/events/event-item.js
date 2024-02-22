ccf80b39c036d3cbb55f413e825066827e64ac37
martashanzl2000 <martin.hanzl@monstermedia.cz>
2024-02-22 19:00:45 +0100

    156. Adding Regular React Components

---
+++ section5/components/events/event-item.js
@@ -0,0 +1,7 @@
+export default function EventItem(props) {
+    return (
+        <li key={props.id}>
+
+        </li>
+    )
+}
--- section5/pages/index.js
+++ section5/pages/index.js
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import {getFeaturedEvents} from "../dummy-data";
 export default function HomePage() {
+    const featuredEvents = getFeaturedEvents();
     return (
         <main>
             <h1>Homepage</h1>
             <ul>
+                {featuredEvents.map(event => {
 
+                })}
             </ul>
         </main>
     )
