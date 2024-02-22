212042cf6a613decc9ab161e3c200fb9eb12fa8f
martashanzl2000 <martin.hanzl@monstermedia.cz>
2024-02-22 19:57:47 +0100

    163. Working on the "All Events" Page

--- section5/pages/events/index.js
+++ section5/pages/events/index.js
@@ -1,7 +1,12 @@
+import {getAllEvents} from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+
 export default function EventsPage() {
+    const events = getAllEvents();
     return (
         <main>
             <h1>Events page</h1>
+            <EventList items={events} />
         </main>
     )
 }
