15b55039601f19e02192f6ca7fc254bb2535d0d1
martashanzl2000 <martin.hanzl@monstermedia.cz>
2024-02-23 19:50:24 +0100

    203. Loading Data & Paths For Dynamic Pages

--- section7/helpers/api-utils.js
+++ section7/helpers/api-utils.js
@@ -1,8 +1,3 @@
-export async function getFeaturedEvents() {
-    const allEvents = await getAllEvents();
-    return allEvents.filter((event) => event.isFeatures);
-}
-
 export async function getAllEvents() {
     const response = await fetch('https://quickstart-1612785053126-default-rtdb.firebaseio.com/events.json');
     const data = await response.json();
@@ -13,23 +8,31 @@ export async function getAllEvents() {
         events.push({
             id: key,
             ...data[key]
-        })
+        });
     }
 
     return events;
 }
 
-export function getFilteredEvents(dateFilter) {
-    const {year, month} = dateFilter;
+export async function getFeaturedEvents() {
+    const allEvents = await getAllEvents();
+    return allEvents.filter((event) => event.isFeatured);
+}
 
-    let filteredEvents = DUMMY_EVENTS.filter((event) => {
+export async function getEventById(id) {
+    const allEvents = await getAllEvents();
+    return allEvents.find((event) => event.id === id);
+}
+
+export async function getFilteredEvents(dateFilter) {
+    const { year, month } = dateFilter;
+
+    const allEvents = await getAllEvents();
+
+    let filteredEvents = allEvents.filter((event) => {
         const eventDate = new Date(event.date);
         return eventDate.getFullYear() === year && eventDate.getMonth() === month - 1;
     });
 
     return filteredEvents;
-}
-
-export function getEventById(id) {
-    return DUMMY_EVENTS.find((event) => event.id === id);
 }
--- section7/pages/events/[eventId].js
+++ section7/pages/events/[eventId].js
@@ -1,40 +1,60 @@
 import { Fragment } from 'react';
-import { useRouter } from 'next/router';
 
-import { getEventById } from '../../dummy-data';
+import { getEventById, getFeaturedEvents } from '../../helpers/api-utils';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
 
-function EventDetailPage() {
-  const router = useRouter();
+function EventDetailPage(props) {
+    const event = props.selectedEvent;
 
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+    if (!event) {
+        return (
+            <div className="center">
+                <p>Loading...</p>
+            </div>
+        );
+    }
 
-  if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
+        <Fragment>
+            <EventSummary title={event.title} />
+            <EventLogistics
+                date={event.date}
+                address={event.location}
+                image={event.image}
+                imageAlt={event.title}
+            />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
     );
-  }
+}
 
-  return (
-    <Fragment>
-      <EventSummary title={event.title} />
-      <EventLogistics
-        date={event.date}
-        address={event.location}
-        image={event.image}
-        imageAlt={event.title}
-      />
-      <EventContent>
-        <p>{event.description}</p>
-      </EventContent>
-    </Fragment>
-  );
+export async function getStaticProps(context) {
+    const eventId = context.params.eventId;
+
+    const event = await getEventById(eventId);
+
+    return {
+        props: {
+            selectedEvent: event
+        },
+        revalidate: 30
+    };
+}
+
+export async function getStaticPaths() {
+    const events = await getFeaturedEvents();
+
+    const paths = events.map(event => ({ params: { eventId: event.id } }));
+
+    return {
+        paths: paths,
+        fallback: 'blocking'
+    };
 }
 
 export default EventDetailPage;
