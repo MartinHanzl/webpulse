f13fb20e8d22c2b0be990e424279e1a40bd95997
martashanzl2000 <martin.hanzl@monstermedia.cz>
2024-02-23 15:36:04 +0100

    180. A Closer Look At "getStaticProps" & Configuration Options

--- section6/pages/index.js
+++ section6/pages/index.js
@@ -13,15 +13,19 @@ function HomePage(props) {
 export default HomePage;
 
 export async function getStaticProps(context) {
-    console.log('regenerating');
     const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
     const jsonData = await fs.readFile(filePath);
     const data = JSON.parse(jsonData);
 
+    if (data.products.length === 0) {
+        return {notFound: true};
+    }
+
     return {
         props: {
             products: data.products,
-            revalidate: 1 // really matters in production
         },
+        revalidate: 1, // really matters in production
+        //redirect: { destination: 'https://google.com/' }
     };
 }
