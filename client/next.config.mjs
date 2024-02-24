c6dd8edf6678609cc4a00ce2348dfd0bfb45ab29
martashanzl2000 <martin.hanzl@monstermedia.cz>
2024-02-24 11:37:23 +0100

    adding translations

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'cs', 'de'],
+    defaultLocale: 'en',
+    localeDetection: false,
+    domains: [
+      {
+        domain: 'example.com',
+        defaultLocale: 'en',
+        http: true,
+      },
+      {
+        domain: 'example.cz',
+        defaultLocale: 'cs',
+        http: true,
+      },
+      {
+        domain: 'example.de',
+        defaultLocale: 'de',
+        http: true,
+      },
+    ],
+  },
+  trailingSlash: true,
 };
+
 
 export default nextConfig;
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,16 @@
 import "@/styles/globals.css";
 import Header from "@/components/layout/header";
+import {Fragment} from "react";
 
 export default function App({Component, pageProps}) {
     return (
         <>
             <Header/>
-            <Component {...pageProps} />
+            <Fragment>
+                <main>
+                    <Component {...pageProps} />
+                </main>
+            </Fragment>
         </>
     );
 }
