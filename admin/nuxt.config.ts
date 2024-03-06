3e1b09c9d2335c6840ff70155ae567fba4404827
martashanzl2000 <martin.hanzl@monstermedia.cz>
2024-03-06 18:42:10 +0100

    multiple languages and default layout

--- admin/nuxt.config.ts
+++ admin/nuxt.config.ts
@@ -3,6 +3,21 @@ export default defineNuxtConfig({
     devtools: {enabled: true},
     modules: [
         '@nuxtjs/tailwindcss',
-        '@nuxtjs/i18n'
-    ]
+        '@nuxtjs/i18n',
+    ],
+    i18n: {
+        defaultLocale: 'cs',
+        locales: [
+            {
+                code: 'cs',
+                name: 'Čeština',
+                file: './lang/cs.json',
+            },
+            {
+                code: 'en',
+                name: 'English',
+                file: './lang/en.json',
+            }
+        ]
+    }
 })
