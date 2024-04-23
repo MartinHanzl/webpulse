99d11aa91e6cff8978114a95c7bd360558ebbaf0
martashanzl2000 <martin.hanzl@monstermedia.cz>
2024-04-23 17:19:27 +0200

    temporary layout update

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -1,3 +1,15 @@
 <template>
   <div></div>
 </template>
+
+<script setup lang="ts">
+definePageMeta({
+  title: 'Dashboard',
+  description: 'Welcome to the dashboard',
+  image: 'https://example.com/image.jpg',
+  url: 'https://example.com/dashboard',
+  type: 'website',
+  keywords: 'dashboard, admin, panel',
+  layout: 'login',
+});
+</script>
