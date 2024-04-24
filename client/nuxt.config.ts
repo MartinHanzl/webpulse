7fb48e88ea9514acdd83524b7730f2fe6a55addf
martashanzl2000 <martin.hanzl@monstermedia.cz>
2024-04-24 18:42:17 +0200

    client init

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,35 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {enabled: true},
-    modules: [
-        "@nuxtjs/tailwindcss",
-        '@nuxtjs/i18n',
-        '@nuxtjs/eslint-module',
-        "@nuxt/ui",
-        '@zadigetvoltaire/nuxt-gtm',
-        'nuxt-gtag',
-        'nuxt-headlessui',
-        '@nuxtjs/seo'
-    ],
-    eslint: {
-        checker: true,
-    },
-    pinia: {
-        storesDirs: ['./stores/**'],
-    },
-    i18n: {
-        defaultLocale: 'cs',
-        locales: [
-            {
-                code: 'cs',
-                name: 'Čeština',
-                file: './lang/cs.json',
-            },
-            {
-                code: 'en',
-                name: 'English',
-                file: './lang/en.json',
-            }
-        ]
-    }
+  devtools: { enabled: true }
 })
