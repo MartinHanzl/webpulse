2245ee7a206d9958b455a96a16485152e3f48aee
martashanzl2000 <martin.hanzl@monstermedia.cz>
2024-11-29 15:05:26 +0100

    users update

--- server/app/Http/Controllers/User/UserController.php
+++ server/app/Http/Controllers/User/UserController.php
@@ -73,8 +73,8 @@ class UserController extends Controller
         $validator = Validator::make($request->all(), [
             'firstname' => 'required|string|max:255',
             'lastname' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users:except,' . $id,
-            'phone' => 'required|string|max:255|unique:users:except,' . $id,
+            'email' => 'required|string|email|max:255|unique:users:email,' . $id,
+            'phone' => 'required|string|max:255|unique:users:email,' . $id,
         ]);
 
         if ($validator->fails()) {
