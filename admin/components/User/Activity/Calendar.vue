0fc378dd00a22ba3ceb940b1276181eb82204c00
martashanzl2000 <martin.hanzl@monstermedia.cz>
2025-01-12 13:07:28 +0100

    responsiveness update

--- admin/components/User/Activity/Calendar.vue
+++ admin/components/User/Activity/Calendar.vue
@@ -115,7 +115,7 @@ const activitiesByDay = computed(() => {
 </script>
 
 <template>
-	<div class="p-4">
+	<div>
 		<div class="flex justify-between items-center mb-4">
 			<BaseButton
 				variant="primary"
@@ -124,7 +124,7 @@ const activitiesByDay = computed(() => {
 			>
 				Předchozí měsíc
 			</BaseButton>
-			<h2 class="text-lg font-semibold text-primaryCustom">
+			<h2 class="text-sm lg:text-md font-semibold text-primaryCustom">
 				{{ currentDate.toLocaleDateString('cs-CZ', { month: 'long', year: 'numeric' })[0].toUpperCase() + currentDate.toLocaleDateString('cs-CZ', { month: 'long', year: 'numeric' }).slice(1) }}
 			</h2>
 			<BaseButton
@@ -136,31 +136,31 @@ const activitiesByDay = computed(() => {
 			</BaseButton>
 		</div>
 		<div class="grid grid-cols-7">
-			<div class="text-center py-2 font-semibold bg-grayDark rounded-l-lg">
+			<div class="text-xs lg:text-md text-center py-1.5 lg:py-2 font-semibold bg-grayDark rounded-l-lg">
 				Po
 			</div>
-			<div class="text-center py-2 font-semibold bg-grayDark">
+			<div class="text-xs lg:text-md text-center py-1.5 lg:py-2 font-semibold bg-grayDark">
 				Út
 			</div>
-			<div class="text-center py-2 font-semibold bg-grayDark">
+			<div class="text-xs lg:text-md text-center py-1.5 lg:py-2 font-semibold bg-grayDark">
 				St
 			</div>
-			<div class="text-center py-2 font-semibold bg-grayDark">
+			<div class="text-xs lg:text-md text-center py-1.5 lg:py-2 font-semibold bg-grayDark">
 				Čt
 			</div>
-			<div class="text-center py-2 font-semibold bg-grayDark">
+			<div class="text-xs lg:text-md text-center py-1.5 lg:py-2 font-semibold bg-grayDark">
 				Pá
 			</div>
-			<div class="text-center py-2 font-semibold bg-grayDark">
+			<div class="text-xs lg:text-md text-center py-1.5 lg:py-2 font-semibold bg-grayDark">
 				So
 			</div>
-			<div class="text-center py-2 font-semibold bg-grayDark rounded-r-lg">
+			<div class="text-xs lg:text-md text-center py-1.5 lg:py-2 font-semibold bg-grayDark rounded-r-lg">
 				Ne
 			</div>
 			<div
 				v-for="n in startOfMonth"
 				:key="n"
-				class="p-4"
+				class="p-2 lg:p-4"
 			/>
 			<div
 				v-for="day in days"
@@ -168,12 +168,12 @@ const activitiesByDay = computed(() => {
 				class="min-h-[128px] h-auto p-2 flex flex-col border border-gray-100"
 			>
 				<div
-					:class="['flex size-6 items-center justify-center text-primaryCustom rounded-full cursor-pointer font-medium p-[16px]', { 'bg-primaryCustom text-white': day.toDateString() === selectedDate.toDateString() }]"
+					:class="['flex size-2 lg:size-6 items-center justify-center text-primaryCustom rounded-full cursor-pointer font-medium p-[12px] lg:p-[16px] text-xs lg:text-sm', { 'bg-primaryCustom text-white': day.toDateString() === selectedDate.toDateString() }]"
 					@click="selectDate(day)"
 				>
 					{{ day.getDate() }}
 				</div>
-				<div class="mt-4 grid grid-cols-3 gap-4">
+				<div class="mt-4 grid grid-cols-1 lg:grid-cols-3 gap-4">
 					<div
 						v-for="(activityItem, index) in activitiesByDay(day)"
 						:key="index"
@@ -182,37 +182,37 @@ const activitiesByDay = computed(() => {
 					>
 						<div
 							v-if="[6, 8, 11, 12].includes(activityItem.activity.id) && !activityItem.completed"
-							class="p-4 border-b-2 border-r-2 border-danger"
+							class="p-2 lg:p-4 border-b-2 border-r-2 border-danger"
 						/>
 						<div
 							v-else-if="[6, 8, 11, 12].includes(activityItem.activity.id) && activityItem.completed"
-							class="p-4 border-2 border-danger bg-danger"
+							class="p-2 lg:p-4 border-2 border-danger bg-danger"
 						/>
 						<PauseCircleIcon
 							v-if="activityItem.activity.id === 10"
-							class="text-danger size-8"
+							class="text-danger size-5 lg:size-8"
 						/>
 						<StarIcon
 							v-if="activityItem.activity.id === 22"
-							class="text-danger size-8 col-span-1 fill-danger"
+							class="text-danger size-5 lg:size-8 col-span-1 fill-danger"
 						/>
 						<div
 							v-if="activityItem.activity.id === 21"
-							class="p-4 border-2 border-grayDark col-span-1"
+							class="p-2 lg:p-4 border-2 border-grayDark col-span-1"
 						/>
 						<div
 							v-if="[13, 14, 23].includes(activityItem.activity.id)"
-							class="p-4 border-2 border-primaryLight col-span-1"
+							class="p-2 lg:p-4 border-2 border-primaryLight col-span-1"
 						/>
 						<div
 							v-if="activityItem.activity.id === 18"
-							class="flex items-center justify-center col-span-1 text-success font-semibold"
+							class="flex items-center justify-center col-span-1 text-md lg:text-lg text-success font-semibold"
 						>
 							K
 						</div>
 						<div
 							v-if="activityItem.activity.id === 20"
-							class="flex items-center justify-center col-span-1 text-primaryLight font-semibold"
+							class="flex items-center justify-center col-span-1 text-md lg:text-lg text-primaryLight font-semibold"
 						>
 							K
 						</div>
@@ -223,35 +223,35 @@ const activitiesByDay = computed(() => {
 					>
 						<SmileBookIcon
 							v-if="checkSmile(day) === 'book'"
-							class="size-8 fill-success"
+							class="size-5 lg:size-8 fill-success"
 						/>
 						<SmileAudioIcon
 							v-else-if="checkSmile(day) === 'audio'"
-							class="size-8 fill-success"
+							class="size-5 lg:size-8 fill-success"
 						/>
 						<SmileBookIcon
 							v-else-if="checkSmile(day) === 'book'"
-							class="size-8 fill-success"
+							class="size-5 lg:size-8 fill-success"
 						/>
 						<SmileDreamIcon
 							v-else-if="checkSmile(day) === 'dream'"
-							class="size-8 fill-success"
+							class="size-5 lg:size-8 fill-success"
 						/>
 						<SmileAudioBookIcon
 							v-else-if="checkSmile(day) === 'audiobook'"
-							class="size-8 fill-success"
+							class="size-5 lg:size-8 fill-success"
 						/>
 						<SmileBookDreamIcon
 							v-else-if="checkSmile(day) === 'bookdream'"
-							class="size-8 fill-success"
+							class="size-5 lg:size-8 fill-success"
 						/>
 						<SmileAudioDreamIcon
 							v-else-if="checkSmile(day) === 'audiodream'"
-							class="size-8 fill-success"
+							class="size-5 lg:size-8 fill-success"
 						/>
 						<SmileFullIcon
 							v-else-if="checkSmile(day) === 'full'"
-							class="size-8 fill-success"
+							class="size-5 lg:size-8 fill-success"
 						/>
 					</div>
 				</div>
