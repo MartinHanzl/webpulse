d9c2574bfc00b66d1e52a6fca8f9925c2d749a7f
martashanzl2000 <martin.hanzl@monstermedia.cz>
2025-04-10 12:42:12 +0200

    minor command update

--- server/app/Console/Commands/Manual/DevCommand.php
+++ server/app/Console/Commands/Manual/DevCommand.php
@@ -13,28 +13,40 @@ class DevCommand extends Command
 
     public function handle()
     {
-        $this->output->title('Start PW Links redirects');
+        $this->output->title('Start Links redirects');
 
         $linksRaw = [];
-        $rows = SimpleExcelReader::create(storage_path('app/pw-hu-links.xlsx'))->getRows();
+        $rows = SimpleExcelReader::create(storage_path('app/etool-redirects.xlsx'))->getRows();
 
         $rows->each(function (array $rowProperties) use (&$linksRaw) {
-            $linksRaw[] = $rowProperties['URL'];
+            $linksRaw[] = [
+                'origin' => $rowProperties['Origin'],
+                'redirect' => $rowProperties['Redirect'],
+            ];
         });
 
         $correctLinks = [];
         $this->output->progressStart(count($linksRaw));
         foreach ($linksRaw as $key => $linkRaw) {
-            $link = str_replace('https://party-world.hu', '', $linkRaw);
-            $path = '/';
-            $correctLinks[] = [
-                'from' => $link,
-                'to' => $path,
-            ];
             $this->output->progressAdvance();
+            $originLink = $linkRaw['origin'];
+            $restrictedValues = ['png', 'jpg', 'jpeg', 'webp', 'gif', 'mp4', 'avi', 'mov', 'mkv', 'flv', 'wmv', 'wp-content', 'css', 'js', 'fonts', 'font', 'woff', 'woff2', 'ttf', 'eot', 'svg', 'ico', 'pdf'];
+            foreach ($restrictedValues as $restrictedValue) {
+                if (str_contains($originLink, $restrictedValue)) {
+                    continue 2;
+                }
+            }
+
+            $linkRaw['redirect'] = $linkRaw['redirect'] == 'CHYBÍ STRÁNKA' ? '/' : $linkRaw['origin'];
+            $linkRaw['redirect'] = str_contains('wp-', $linkRaw['redirect']) ? '/' : $linkRaw['redirect'];
+
+            $correctLinks[] = [
+                'from' => str_replace(['https://www.etool.cz', 'http://www.etool.cz'], '/', $linkRaw['origin']),
+                'to' => $linkRaw['redirect'],
+            ];
         }
         $correctLinks = json_encode($correctLinks, JSON_PRESERVE_ZERO_FRACTION);
-        file_put_contents(storage_path('app/pw-links.json'), $correctLinks);
+        file_put_contents(storage_path('app/etool-links.json'), $correctLinks);
 
         $this->output->progressFinish();
     }
