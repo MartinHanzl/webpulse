255d451edc56498be92b1a0107c8692e07f6ee85
martashanzl2000 <martin.hanzl@monstermedia.cz>
2025-05-24 17:31:51 +0200

    technologies

--- client/components/Home/Technologies.vue
+++ client/components/Home/Technologies.vue
@@ -3,65 +3,65 @@ const { t } = useI18n();
 </script>
 
 <template>
-  <div>
+  <div class="text-center">
     <BasePropsHeading type="h2" class="mb-6 text-center">
       {{ t("home.technologiesTitle") }}
     </BasePropsHeading>
-    <div class="flex justify-center gap-x-6">
+    <div class="flex justify-center flex-wrap gap-x-6">
       <NuxtImg
         src="/static/img/icon/technologies/laravel.svg"
         alt="Laravel"
-        class="w-[50%] h-auto"
+        class="w-[96px] h-auto"
       />
       <NuxtImg
         src="/static/img/icon/technologies/docker.svg"
         alt="Docker"
-        class="w-[50%] h-auto"
+        class="w-[96px] h-auto"
       />
       <NuxtImg
         src="/static/img/icon/technologies/aws.svg"
         alt="Amazon web services"
-        class="w-[50%] h-auto"
+        class="w-[96px] h-auto"
       />
       <NuxtImg
         src="/static/img/icon/technologies/nodejs.svg"
         alt="Node.js"
-        class="w-[50%] h-auto"
+        class="w-[96px] h-auto"
       />
       <NuxtImg
         src="/static/img/icon/technologies/npm.svg"
         alt="Npm"
-        class="w-[50%] h-auto"
+        class="w-[96px] h-auto"
       />
       <NuxtImg
         src="/static/img/icon/technologies/php.svg"
         alt="PHP"
-        class="w-[50%] h-auto"
+        class="w-[96px] h-auto"
       />
       <NuxtImg
         src="/static/img/icon/technologies/nuxt.svg"
         alt="Nuxt.js"
-        class="w-[50%] h-auto"
+        class="w-[96px] h-auto"
       />
       <NuxtImg
         src="/static/img/icon/technologies/js.svg"
         alt="JavaScript"
-        class="w-[50%] h-auto"
+        class="w-[96px] h-auto"
       />
       <NuxtImg
         src="/static/img/icon/technologies/github.svg"
         alt="Github"
-        class="w-[50%] h-auto"
+        class="w-[96px] h-auto"
       />
       <NuxtImg
         src="/static/img/icon/technologies/figma.svg"
         alt="Figma"
-        class="w-[50%] h-auto"
+        class="w-[96px] h-auto"
       />
       <NuxtImg
         src="/static/img/icon/technologies/mysql.svg"
         alt="Mysql"
-        class="w-[50%] h-auto"
+        class="w-[96px] h-auto"
       />
     </div>
   </div>
