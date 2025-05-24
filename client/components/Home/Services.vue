a29d52093bafa2b2393619dd36354cfa02d38d2b
martashanzl2000 <martin.hanzl@monstermedia.cz>
2025-05-25 00:28:17 +0200

    services update

--- client/components/Home/Services.vue
+++ client/components/Home/Services.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import { ref } from "vue";
 const { t } = useI18n();
+
+const expandedIndex = ref<number | null>(null);
 defineProps({
   services: {
     type: Array,
@@ -7,32 +10,65 @@ defineProps({
   },
 });
 </script>
+
 <template>
-  <div>
-    <BasePropsHeading type="h2">
-      {{ t("services.title") }}
-    </BasePropsHeading>
-    <div>
-      <div
-        v-for="(service, index) in services"
-        :key="index"
-        :class="[
-          index % 2 === 0 ? 'text-right' : 'text-left',
-          'border-b border-b-primary py-6',
-        ]"
-      >
-        <BasePropsHeading type="h3">
-          {{ service.name }}
-        </BasePropsHeading>
-        <BasePropsParagraph bold="bold">
-          {{ service.perex }}
-        </BasePropsParagraph>
-        <BasePropsParagraph class="mt-2">
-          Cena:
-          {{ $formatPrice(service.price, service.currency, service.tax_rate) }}
-          (cena za
-          {{ service.price_type === "hourly" ? "hodinu" : "projekt" }})
-        </BasePropsParagraph>
+  <div class="space-y-2">
+    <BasePropsHeading type="h2" class="text-center">{{
+      t("services.title")
+    }}</BasePropsHeading>
+    <div
+      v-for="(service, index) in services"
+      :key="index"
+      @mouseenter="expandedIndex = index"
+      @mouseleave="expandedIndex = null"
+      class="overflow-hidden transition-all duration-500 ease-in-out rounded-lg cursor-pointer border border-dark"
+    >
+      <div class="p-6">
+        <div class="grid grid-cols-6 gap-x-4">
+          <p class="col-span-1">{{ `0${index + 1}` }}</p>
+          <div class="col-span-5">
+            <BasePropsHeading type="h3" margin-bottom="mb-0">{{
+              service.name
+            }}</BasePropsHeading>
+            <div
+              v-if="expandedIndex === index"
+              class="mt-4 opacity-100 transition-opacity duration-500"
+            >
+              <div
+                :class="[
+                  expandedIndex !== index ? 'hidden' : '',
+                  'transition-all duration-500 ease-in-out',
+                ]"
+              >
+                <BasePropsParagraph>{{
+                  service.description
+                }}</BasePropsParagraph>
+                <div class="flex items-center justify-between">
+                  <BasePropsParagraph bold="bold">
+                    Cena:
+                    {{
+                      $formatPrice(
+                        service.price,
+                        service.currency,
+                        service.tax_rate,
+                      )
+                    }}
+                    (cena za
+                    {{
+                      service.price_type === "hourly" ? "hodinu" : "projekt"
+                    }})
+                  </BasePropsParagraph>
+                  <BaseButton
+                    :href="`/services/${service.slug}`"
+                    size="xl"
+                    variant="primary"
+                    >{{ t("services.inquiry") }}</BaseButton
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
