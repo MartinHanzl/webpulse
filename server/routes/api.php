108dcf047c78b30e713d013cb2c5f0532df8488f
martashanzl2000 <martin.hanzl@monstermedia.cz>
2025-05-24 19:19:42 +0200

    services update

--- server/routes/api.php
+++ server/routes/api.php
@@ -25,15 +25,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Currency\CurrencyController;
 use App\Http\Controllers\Admin\Country\CountryController;
 use App\Http\Controllers\Admin\PriceOffer\PriceOfferController;
-use \App\Http\Controllers\Admin\Service\ServiceController;
+use App\Http\Controllers\Admin\Service\ServiceController;
 use App\Http\Controllers\Client\Service\ServiceController as ClientServiceController;
 
 
 Route::group([
     'prefix' => 'service'
 ], function () {
-    Route::get('{lang?}', [ServiceController::class, 'index']);
-    Route::get('{id}/{lang?}', [ServiceController::class, 'show'])->where('id', '[0-9]+');
+    Route::get('{lang?}', [ClientServiceController::class, 'index']);
+    Route::get('{id}/{lang?}', [ClientServiceController::class, 'show'])->where('id', '[0-9]+');
 });
 
 Route::group([
